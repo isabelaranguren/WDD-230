83963237286eb9ecf495a548f6d4315f9aec9895
isabelaranguren <60532846+isabelaranguren@users.noreply.github.com>
2020-11-07 18:14:44 -0600

    Add files via upload

--- lesson8/js/app.js
+++ lesson8/js/app.js
@@ -1,27 +1,45 @@
-function main() {
-    getDate();
-    displayBanner();
-    getWindChill();
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
 
+const imgOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -100px 0px"
+};
+
+if('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if(item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
 }
 
-function getDate() {
-    var d = new Date();
-    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    var month = d.getMonth();
-    var weekday = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    var dayOfWeek = d.getDay();
-    var day = d.getDate();
-    var year = d.getFullYear();
-    var currentDay = `Today is ${weekday[dayOfWeek-1]}, ${day} ${months[month]} ${year} `
-    document.getElementById('currentDate').innerHTML = currentDay;
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
-function displayBanner(){
-    var dayOfWeek = false
-    if (new Date().getDay() == "5" || dayOfWeek == true){
-        document.getElementById('banner').classList.toggle('hidden');
-    }
-    document.documentElement.scrollTop = 0;
+function updateSeverity(val) {
+  document.getElementById('severitydisplay').innerHTML = "Severity: " + val; 
 }
-
