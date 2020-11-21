673f54ef82e3140ad60ba1f1473993b06f2d29ad
isabelaranguren <60532846+isabelaranguren@users.noreply.github.com>
2020-11-20 19:40:05 -0600

    Update weatherapi.js

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,17 +1,17 @@
-const apiURL = "http://api.openweathermap.org/preston/2.5/forecast?id=5604473&units=imperial&appid=8ad3323b6bd5bd5cba6cbb7707444f1c";
 
+
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=8ad3323b6bd5bd5cba6cbb7707444f1c';
 fetch(apiURL)
-  .then((response) => response.json())
-  .then((preston) => {
-    console.log(preston);
-
-    document.getElementById('current-temp').innerHTML = preston.main.temp;
-    const imagesrc = 'https://openweathermap.org/img/w/' + preston.weather[0].icon + '.png'; 
-    const desc = preston.weather[0].description;  
-    document.getElementById('imagesrc').textContent = imagesrc;  
-    document.getElementById('icon').setAttribute('src', imagesrc);  
-    document.getElementById('icon').setAttribute('alt', desc);
-
-  }
+.then((response) => response.json())
+.then((preston) => {
   
+document.getElementById('current-temp').textContent = preston.main.temp;
+
+const imagesrc = 'https://openweathermap.org/img/w/' + preston.weather[0].icon + '.png'; 
+const desc = preston.weather[0].description;  
+document.getElementById('imagesrc').textContent = imagesrc;  
+document.getElementById('icon').setAttribute('src', imagesrc);  
+document.getElementById('icon').setAttribute('alt', desc);
+  
+}
   );
