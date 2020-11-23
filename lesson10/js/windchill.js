65bcc27f63fe2d0db9adc2b7ad22f0e59bb2d6cc
isabelaranguren <60532846+isabelaranguren@users.noreply.github.com>
2020-11-22 20:46:34 -0600

    Delete windchill.js

--- lesson10/js/windchill.js
+++
@@ -1,7 +0,0 @@
-function getWindchill() {
-    const temperature = parseInt(document.getElementById('currTemp').innerText)
-    const windSpeed = parseInt(document.getElementById('windSpeed').innerText)
-    const windchill = 35.74 + (0.6215* temperature) - (35.75 * Math.pow(windSpeed, 0.16) ) + (0.4275 * temperature * Math.pow(windSpeed, 0.16))
-    document.getElementById('windChill').innerText = windchill.toFixed(2)
-}
-getWindchill()
