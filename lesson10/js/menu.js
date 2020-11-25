701d5d185a0e42dde70e54fd4b63e41fce445f4e
isabelaranguren <60532846+isabelaranguren@users.noreply.github.com>
2020-11-25 15:19:11 -0600

    Delete menu.js

--- lesson10/js/menu.js
+++
@@ -1,4 +0,0 @@
-const hamBtn = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation')
-
-hamBtn.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
