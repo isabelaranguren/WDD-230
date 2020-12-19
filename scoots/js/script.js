06823d73efe286fecc0bede90674223a1a8d0853
isabelaranguren <60532846+isabelaranguren@users.noreply.github.com>
2020-12-18 19:48:51 -0600

    Update script.js

--- scoots/js/script.js
+++ scoots/js/script.js
@@ -213,3 +213,20 @@ function initMap() {
        });
     }
  }
+
+d = new Date
+document.getElementById('rStart').min = `${d.getMonth()+1}-${d.getDate()}-${d.getFullYear()}`;
+
+document.getElementById('rEnd').min = `${d.getMonth()+1}-${d.getDate()+1}-${d.getFullYear()}`;
+
+fetch("data/types.json")
+  .then(response => response.json())
+  .then(rentals => {
+    for (let i = 0; i < rentals.prices.length; i++) {
+      option = document.createElement("option");
+      option.setAttribute("value", rentals.prices[i].rental_type);
+      option.textContent = rentals.prices[i].rental_type;
+
+      document.getElementById('vehicleType').appendChild(option);
+    }
+  });
